9da54760106a8049ea26eb9126c5d4fefa7ae3a7
Ismary <ismary.salges@corebiz.ag>
2021-09-19 16:01:32 -0300

    se coloca videos con javascripy

--- js/app.js
+++ js/app.js
@@ -43,3 +43,55 @@ function clear() {
   document.getElementById("container-user").innerHTML = "";
   document.getElementById("load").innerHTML = "<h2 class='load'>Cargando...</h2>";
 }
+
+
+
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+ 
+
+let player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+      height : '100%',
+      width : '100%',
+      videoId : 'fOW8Y09GVek', 
+      playerlets : { 
+          'autoplay' : 1, 
+          'controls' : 0,
+          'autohide' : 1, 
+          'showinfo' : 0, 
+          'modestbranding' : 0, 
+          'rel' : 0 
+      },
+      events : {
+          'onReady': onPlayerReady, 
+          'onStateChange': onPlayerStateChange 
+      }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo()
+}
+
+let done = false;
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.ENDED) {
+      done = true;
+  }
+  if (event.data == YT.PlayerState.PAUSED) {
+  }
+}
+
+function playvideo(){
+  player.playVideo();
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
